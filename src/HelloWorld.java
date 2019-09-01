3481b087ca5d6be234109a82c6dc32e0cd56c832
fengyangwei <1014@qq.com>
2019-09-01 15:48:50 +0800

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.method1();
+
+    }
+
+    public void method() {
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,15 @@
+/**
+ * @author feng
+ * @create 2019-09-01
+ */
+public class Person {
+    public void method1(){
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
